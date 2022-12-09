64992b0805760cb967e24d0ba0d666a3513cd901
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-09 10:52:29 -0700

    Updates

---
+++ precache-manifest.040e015e9bdb0fc32f0c6d5997944d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f51e52426b4364f8c239cadac83ba8",
+    "url": "/dark-mode-react/index.html"
+  },
+  {
+    "revision": "fd6a551a3907df9e541a",
+    "url": "/dark-mode-react/static/css/main.3c98242c.chunk.css"
+  },
+  {
+    "revision": "4646610705680ce7d806",
+    "url": "/dark-mode-react/static/js/2.a99e8b18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/dark-mode-react/static/js/2.a99e8b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6a551a3907df9e541a",
+    "url": "/dark-mode-react/static/js/main.ba4b879e.chunk.js"
+  },
+  {
+    "revision": "65936b88a9442a948052",
+    "url": "/dark-mode-react/static/js/runtime-main.05c39f3a.js"
+  }
+]);
